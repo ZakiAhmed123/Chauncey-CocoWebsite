cba3f050613c36ef46ff9c24f68690dc7975ac11
“Zaki <reach.zaki.ahmed@gmail.com>
2016-07-02 19:58:31 -0500

    fixed styling added contact about us pages

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
   $('p.title').hide().fadeIn(2000).delay(1000);
-  $('img').hide().fadeIn(3000).delay(2000);
+  $('.welcome-container img').hide().fadeIn(3000).delay(2000);
   $('p.subtitle').hide().fadeIn(4000).delay(3000);
 });
